6df1cba9569966793f7b723451fcb2cc5f77871d
Alexey Makarov <pwuser@yandex.ru>
2023-10-18 23:24:42 +0500

    first commit

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.get('/domains', (req, res) => {
+  const domainList = ["https://personal.mts.ru"];
+  res.json(domainList);
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
